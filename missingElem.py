f3155c9f673269ed61b1dc3178c1046722018b80
Arishkumar404 <arishkumar404.s@gmail.com>
2024-11-24 09:57:27 +0530

    Add files via upload

---
+++ missingElem.py
@@ -0,0 +1,17 @@
+a = [25,10,5,20,35,30]
+
+for i in range(len(a)):
+    for j in range(len(a)):
+        if a[i]<a[j]:
+            a[i] = a[i]^a[j]
+            a[j] = a[i]^a[j]
+            a[i] = a[i]^a[j]
+print(a)
+div = a[1]-a[0]
+for i in range(len(a)-1):
+    if a[i+1]-a[i]!=div:
+        print (a[i]+div)
+        break
+
+
+
