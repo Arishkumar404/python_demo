c12d3fdda45d462902d81867096835a0f6a95b9e
Arishkumar404 <arishkumar404.s@gmail.com>
2024-11-25 18:48:56 +0530

    Update missingElem.py

--- missingElem.py
+++ missingElem.py
@@ -6,6 +6,12 @@ for i in range(len(a)):
             a[i] = a[i]^a[j]
             a[j] = a[i]^a[j]
             a[i] = a[i]^a[j]
+for i in range(len(a)):
+    for j in range(len(a)):
+        if a[i]<a[j]:
+            a[i] = a[i]^a[j]
+            a[j] = a[i]^a[j]
+            a[i] = a[i]^a[j]
 print(a)
 div = a[1]-a[0]
 for i in range(len(a)-1):
