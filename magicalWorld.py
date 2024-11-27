5ffbd3962c2af4a2b46b34d2fb37f9df35c4d6f3
Arishkumar404 <arishkumar404.s@gmail.com>
2024-11-27 11:11:30 +0530

    Update magicalWorld.py

--- magicalWorld.py
+++ magicalWorld.py
@@ -1,7 +1,7 @@
 n = int(input())
 for i in range(n):
     l = list(map(int,input().split()))
-    if l[0]*l[2]+l[1]>=l[3]:
+    if l[0]*l[2]+l[1]<=l[3]:
         print("YES")
         print("no")
     else:
